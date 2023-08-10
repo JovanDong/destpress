fa5e1fca19d0b28c1d091c2554cef08741b350dd
dongyayun <dongyayun@julanling.com>
2023-08-10 16:09:55 +0800

    feat:技术博客初始化&&配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,55 +1,84 @@
 module.exports = {
-  //这里面路径最开始的/是指向.vuepress/public/的
-  base: '/', //部署站点的基础路径,默认/
-  lang: 'zh-CN', //语言设置
-  title: '你好， VuePress ！', //所有页面标题的后缀，并且在默认主题的导航栏中显示
-  description: '这是我的第一个 VuePress 站点', //站点描述，它会被每个页面的 Frontmatter 中的 description 字段覆盖
-  head: [['link', { rel: 'icon', href: './images/logo.png' }]], //站点头部的icon
-  // themeConfig: {
-  //   logo: './images/logo.png',
-  //   nav: [
-  //     { text: '首页', link: '/' },
-  //     {
-  //       text: '测试知识库',
-  //       link: '/test/',
-  //     },
-  //     {
-  //       text: '相关链接',
-  //       items: [
-  //         {
-  //           text: '测试百度的链接',
-  //           link: 'https://www.baidu.com',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  //   sidebar: {
-  //     '/test/': [
-  //       {
-  //         title: '首页',
-  //         path: '/',
-  //       },
-  //       {
-  //         title: '基础学习',
-  //         path: '/test/first',
-  //         children: [
-  //           { title: '第一个', path: '/test/first' },
-  //           { title: '第二个', path: '/test/second' },
-  //         ],
-  //       },
-  //     ],
-  //     '/中文测试/': [
-  //       {
-  //         title: '首页',
-  //         path: '/',
-  //       },
-  //       {
-  //         title: '中文文件夹测试',
-  //         path: '/中文测试/第一个',
-  //         children: [{ title: '第一个', path: '/中文测试/第一个' }],
-  //       },
-  //     ],
-  //   },
-  //   sidebarDepth: 1, //侧边栏自动提取文章的几层标题
-  // },
+  head: [
+    // 设置 favor.ico，html/head 标题中添加 style 标签
+    ['link', { rel: 'icon', href: `logo.png` }],
+  ],
+  title: 'Jovan的技术站', // 设置网站标题
+  description: '描述：我的技术站',
+  base: '/', //默认路径
+  themeConfig: {
+    // 主题设置
+    nav: [
+      {
+        // 右上导航航条 docs/.vuepress 文件夹下
+        text: '概述',
+        link: '/',
+      },
+      {
+        text: 'JS 学习笔记',
+        items: [
+          { text: '笔记', link: '/guide/vue/test01' }, // 可不写后缀 .md
+          { text: '其它链接', link: 'https://blog.csdn.net/Mr_dong_ya_yun' }, // 外部链接
+        ],
+      },
+      {
+        text: 'Vue 学习笔记',
+        items: [
+          { text: '笔记', link: '/guide/vue/test01' }, // 可不写后缀 .md
+          { text: '其它链接', link: 'https://blog.csdn.net/Mr_dong_ya_yun' }, // 外部链接
+        ],
+      },
+      {
+        text: 'Typescript 学习笔记',
+        items: [
+          { text: '笔记', link: '/guide/ts/' }, // 以 ‘/’结束，默认读取 README.md
+          { text: '其它链接', link: 'https://blog.csdn.net/Mr_dong_ya_yun' }, // 外部链接
+        ],
+      },
+      {
+        text: 'gitCode',
+        link: 'https://gitcode.net/Mr_dong_ya_yun',
+      },
+    ],
+    sidebar: {
+      //左侧列表
+      '/guide/vue/': [
+        {
+          // 对应导航中的link文件夹路径，注意这里是 ‘/’结束
+          title: 'Vue 学习',
+          collapsable: true,
+          children: [
+            {
+              title: '测试01',
+              path: 'test01',
+            },
+            {
+              title: '测试02',
+              path: 'test02',
+            },
+            {
+              title: '测试03',
+              path: 'test03',
+            },
+          ],
+        },
+      ],
+      '/guide/ts/': [
+        {
+          title: 'Typescript 学习',
+          collapsable: false,
+          children: [
+            {
+              title: '测试',
+              path: 'test01',
+            },
+          ],
+        },
+      ],
+      // fallback 侧边栏被最后定义
+      '/': [''], //不能放在数组第一个，否则会导致右侧栏无法使用
+    },
+    // 左侧列表展开级数，默认是 1
+    sidebarDepth: 6,
+  },
 }
